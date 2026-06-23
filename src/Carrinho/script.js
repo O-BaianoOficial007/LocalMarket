cd539da7e3e650341e1eb7871aebc8eb79cc5549
O-BaianoOficial007 <arthurandradepereira7@gmail.com>
2026-06-23 11:48:33 -0300

    Add files via upload

--- src/Carrinho/script.js
+++ src/Carrinho/script.js
@@ -37,5 +37,32 @@ function gerar(){
     const totalElement = document.getElementById("total")
     const listaClone = listaElement.cloneNode(true)
     $(listaClone).find("button").remove()
-    const listaHtml = 
+    const listaHtml = listaClone.innerHTML
+    const totalHtml = totalElement.innerHTML
+    const conteudoHTML = `
+        <html>
+            <head>
+                <meta charset="UTF-8>
+            </head>
+            <body>
+                <h1>PEDIDO CONFIRMADO</h1>
+                <h3>Agradecemos sua compra e sua preferência.</h3>
+                <br>
+                ${listaHtml}
+                <br>
+                <br>
+                ${totalHtml}
+            </body>
+        </html>
+    `
+    const blob = new Blob([conteudoHTML], {type: "application/msword"})
+    const link = document.createElement("a")
+
+    link.href = URL.createObjectURL(blob)
+    link.download = "pedido.doc"
+    link.click()
+    document.getElementById("pedido").style.display = "block"
+}
+function sucessClose(){
+    document.getElementById("pedido").style.display = "none"
 }
